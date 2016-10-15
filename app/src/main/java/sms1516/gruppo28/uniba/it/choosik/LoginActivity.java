dc6a1ae91b5f3dfd35c356965b7cae2dca44403e
ivan <ivan.ardillo@gmail.com>
2016-10-15 15:06:15 +0200

    prima commit

---
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/LoginActivity.java
@@ -0,0 +1,13 @@
+package sms1516.gruppo28.uniba.it.choosik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
