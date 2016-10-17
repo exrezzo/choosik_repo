b3162648e5cd5c5a43e8db0c223968a5bb471464
exrezzo <exrezzo@users.noreply.github.com>
2016-10-17 10:45:09 +0200

    #LoginActivity.java #menu

--- app/src/main/java/sms1516/gruppo28/uniba/it/choosik/LoginActivity.java
+++ app/src/main/java/sms1516/gruppo28/uniba/it/choosik/LoginActivity.java
@@ -1,13 +1,39 @@
 package sms1516.gruppo28.uniba.it.choosik;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameField,passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usernameField = (EditText)findViewById(R.id.txtUser);
+        passwordField = (EditText)findViewById(R.id.txtPassword);
+        final Button loginbtn = (Button) findViewById(R.id.loginbtn);
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+                //controllo credenziali login
+            }
+        });
+
+
     }
+    public void login(View view){
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        new SigninTask(this).execute(username,password);
+
+    }
+
 }
